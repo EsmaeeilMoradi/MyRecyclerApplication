9893fdb5d75f0b729451f4d39d2dd6bb211e7f60
EsmaeeilMoradi <esmaeeilmoradi700@gmail.com>
2023-08-13 11:05:43 +0330

    Designing UI.

---
+++ app/src/main/java/com/esm/myrecyclerapplication/model/EmployeeRole.kt
@@ -0,0 +1,9 @@
+package com.esm.myrecyclerapplication.model
+
+enum class EmployeeRole {
+    HumanResources, Security, Technology
+}
+
+enum class CarModel {
+    BMW, Mercedes, VW, Ford, Doge, Fit
+}
---
+++ app/src/main/java/com/esm/myrecyclerapplication/model/EmployeeUiModel.kt
@@ -0,0 +1,10 @@
+package com.esm.myrecyclerapplication.model
+
+data class EmployeeUiModel(
+    val name: String,
+    val biography: String,
+    val role: String,
+    val carModel: String,
+    val imageUrl: String
+) {
+}
